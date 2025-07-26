ce40f2cb60c28deeca01fba6de88c39f0c34eecb
ariebj <ariebj@gmail.com>
2025-07-26 13:12:42 +0700

    commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,28 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">
+					{{ __('Dashboard') }}
+				</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+				<div class="card-body">
+					@if (session('status'))
+					<div class="alert alert-success" role="alert">
+						{{ session('status') }}
+					</div>
+					@endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+					{{ __('I am using MD Bootstrap CSS Framework') }}
+				</div>
+				<div class="card-footer text-center">
+					<button class="btn btn-primary">MDB Button</button>
+					<button class="btn btn-dark">BTN DARK</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,10 @@
             @yield('content')
         </main>
     </div>
+    <!-- MDB -->
+<script
+  type="text/javascript"
+  src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/9.1.0/mdb.umd.min.js"
+></script>
 </body>
 </html>
